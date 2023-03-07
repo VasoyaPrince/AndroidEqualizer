26483e657add1b151eed59781e327c4159b731ec
Prince Vasoya <prince.vasoya@gloryautotech.com>
2023-03-07 12:43:40 +0530

    Initial commit

---
+++ app/src/main/java/glory/android/Equalizer/EqualizerSettings.java
@@ -0,0 +1,8 @@
+package glory.android.Equalizer;
+
+public class EqualizerSettings {
+    public int[] seekbarpos = new int[5];
+    public int presetPos;
+    public short reverbPreset;
+    public short bassStrength;
+}
